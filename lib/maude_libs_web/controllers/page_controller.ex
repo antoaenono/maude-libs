a2dc942f4d688f02f2a93fbaf20ff85a3ed3d2eb
Anton Bilbaeno <antoaenono@pm.me>
2026-02-28 10:36:41 -0800

    chore: phoenix scaffold

---
+++ lib/maude_libs_web/controllers/page_controller.ex
@@ -0,0 +1,7 @@
+defmodule MaudeLibsWeb.PageController do
+  use MaudeLibsWeb, :controller
+
+  def home(conn, _params) do
+    render(conn, :home)
+  end
+end
---
+++ test/maude_libs_web/controllers/error_json_test.exs
@@ -0,0 +1,12 @@
+defmodule MaudeLibsWeb.ErrorJSONTest do
+  use MaudeLibsWeb.ConnCase, async: true
+
+  test "renders 404" do
+    assert MaudeLibsWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500" do
+    assert MaudeLibsWeb.ErrorJSON.render("500.json", %{}) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
